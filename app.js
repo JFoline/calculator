eca67b839e8ac767f319e8aa1ac2ad3f18512f71
John Foline <john.foline@gmail.com>
2018-03-24 13:19:37 -0500

    split branch to save advanced functions and dispaly for later

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
-let input = document.getElementById("input");
-let output = document.getElementById("output");
+let input = document.getElementById('input');
+let output = document.getElementById('output');
+let runMemory = '';
+let digit = '0';
+let displayValue = '';
 
 var add = (x,y) => {return(x + y)}
 var divide = (x,y) => {return(x - y)}
 var multiply = (x,y) => {return(x * y)}
 var divide = (x,y) => {return(x / y)}
 var operate = (operator, x, y) => { return operator(x ,y)}
-let x ='5';
-let y ='9';
-output.innerHTML = y;
-console.log(multiply(x,y));
+let x =5;
+let y =-9;
+
+
+
+var clearAll = () =>
+  {
+    input.innerHTML = '';
+  }
+
+var typeDig = (digit) =>
+  {
+    input.innerHTML += digit;
+  }
+
+var funcSelect = (symbol) =>
+  {
+    switch (symbol) {
+      case '+':
+
+        break;
+      default:
+
+    }
+  }
+
+var operateTest = () =>
+  {
+
+  }
